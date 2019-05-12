d27ada6f70c8e44881aa8cca15c82be2e7d21b4f
deathflame1515 <ernest.mangune@gmail.com>
2019-05-12 18:04:43 +0800

    ccsarchive

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # SCRIPT TO AUTOMATE SERVER SETUP
 
-# init packages
-# sudo yum install httpd -y
-# sudo yum install mysql-server -y 
-# sudo yum install php-mysql php-ftp php -y
-# yum groupinstall "Development tools" -y
-# sudo yum install git -y
+init packages
+sudo yum install httpd -y
+sudo yum install mysql-server -y 
+sudo yum install php-mysql php-ftp php -y
+yum groupinstall "Development tools" -y
+sudo yum install git -y
 
 service networking restart
 service httpd restart
