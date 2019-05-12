014b2e73f3d1d050c8528b3fa14425059a7cc20a
deathflame1515 <ernest.mangune@gmail.com>
2019-05-12 17:55:59 +0800

    ccsarchive

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SCRIPT TO AUTOMATE SERVER SETUP
+
+# init packages
+# sudo yum install httpd -y
+# sudo yum install mysql-server -y 
+# sudo yum install php-mysql php-ftp php -y
+# yum groupinstall "Development tools" -y
+# sudo yum install git -y
+
+service networking restart
+service httpd restart
+service mysqld restart
+mysql -u root < dblibrary.sql
