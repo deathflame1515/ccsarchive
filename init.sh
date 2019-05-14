f68d9930fe022a36f95b227362c60ef834003391
deathflame1515 <ernest.mangune@gmail.com>
2019-05-14 16:44:06 +0800

    updated script

--- init.sh
+++ init.sh
@@ -8,6 +8,10 @@ sudo yum install php-mysql php-ftp php -y
 yum groupinstall "Development tools" -y
 sudo yum install git -y
 
+systemctl enable httpd.service
+systemctl start httpd.service
+systemctl enable mysqld.service
+systemctl start mysqld.service
 systemctl restart network
 systemctl restart httpd
 mysql -u root -p < dblibrary.sql
